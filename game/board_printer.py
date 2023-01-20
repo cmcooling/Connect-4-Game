cb701b082fecf168b21beb96733453f5110c38c1
coolernato <cmcooling@gmail.com>
2023-01-20 15:32:33 +0000

    Board printer now uses 1s and 2s

--- game/board_printer.py
+++ game/board_printer.py
@@ -5,12 +5,16 @@ def print_board(board):
     for i_row in range(5, -1, -1):
         row = "|"
         for i_column in range(7):
-            if board[i_column][i_row] == 1:
-                row += " X"
-            elif board[i_column][i_row] == 2:
-                row += " O"
+            if board[i_column][i_row]:
+                row += " {}".format(board[i_column][i_row])
             else:
                 row += "  "
+            # if board[i_column][i_row] == 1:
+            #    row += " X"
+            # elif board[i_column][i_row] == 2:
+            #    row += " O"
+            # else:
+            #    row += "  "
         row += " |"
         print(row)
 
