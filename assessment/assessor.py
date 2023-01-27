4bdb66ee4dffa5e97d1c935a7b03f6324d66debf
coolernato <cmcooling@gmail.com>
2023-01-27 16:02:54 +0000

    Added progress inidication

--- assessment/assessor.py
+++ assessment/assessor.py
@@ -16,6 +16,8 @@ def assess(student_strategy):
     forfeits = 0
 
     for i_match in range(n_match):
+        print("Beginning game {} of {}.".format(i_match + 1, n_match), end="\r")
+
         result = run_game("Student", "Opponent", student_strategy, random_opponent, print_output=False, max_move_time=1, randomise_first_player=True)
 
         if result[0] == 1:
@@ -26,6 +28,8 @@ def assess(student_strategy):
             forfeits += 1
         else:
             losses += 1
+
+    print(" " * 100)
 
     mark = 0.25 * (n_match - forfeits) / n_match + 0.75 * ((wins + 0.5 * draws) / n_match) ** 3
 
